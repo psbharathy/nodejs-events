fd8685def9e5a8f740cb385a0119efdd16bfb585
Arshad <arshad@payjo.co>
2018-09-19 11:22:50 +0530

    code refactor

--- controllers/events.js
+++ controllers/events.js
@@ -39,6 +39,47 @@ const createEvent = async (req, res) => {
   res.send(req.body);
 };
 
+const getActorsEvent = async (actorId, req, res) => {
+  console.log("Actors Event");
+
+  try {
+    let query =
+      "SELECT events.id, events.created_at,events.event_type as type,repo.name as repo_name,repo.repo_url as repo_url,repo.id as repo_id,actors.avatar_url as avatar_url,actors.login as login, actors.id as actor_id FROM events LEFT JOIN actors on actors.id = events.actor_id LEFT JOIN repository as repo on repo.id=events.repo_id where events.actor_id = ? Order By events.id ASC";
+    await db.query(query, [actorId], function(err, rows) {
+      // res.status(200).render("events", { title: "All Events", events: rows });
+      const events = eventTransformers(rows);
+
+      res.send(events);
+
+      if (!err) console.log("The solution is: ");
+      else console.log("Error while performing Query.");
+    });
+  } catch (ex) {
+    return ex;
+  }
+};
+
+const deleteEvents = async (req, res) => {
+  try {
+    let query = " TRUNCATE TABLE events";
+    let actQuery = " TRUNCATE TABLE actors";
+    let repoQuery = " TRUNCATE TABLE repository";
+    await db.execute(query, function(err, result) {
+      if (err) throw err;
+      console.log(" Events Records Deleted !");
+    });
+    await db.execute(actQuery, function(err, result) {
+      console.log(" Actors Records Deleted !");
+    });
+    await db.execute(repoQuery, function(err, result) {
+      console.log(" Repository Records Deleted !");
+    });
+    res.send(true);
+  } catch (ex) {
+    return ex;
+  }
+};
+
 function eventTransformers(eventData) {
   const events = [];
   for (let e in eventData) {
@@ -61,46 +102,6 @@ function eventTransformers(eventData) {
   }
   return events;
 }
-const getActorsEvent = async (actorId, req, res) => {
-  console.log("Actors Event");
-
-  try {
-    let query =
-      "SELECT events.id, events.created_at,events.event_type as type,repo.name as repo_name,repo.repo_url as repo_url,repo.id as repo_id,actors.avatar_url as avatar_url,actors.login as login, actors.id as actor_id FROM events LEFT JOIN actors on actors.id = events.actor_id LEFT JOIN repository as repo on repo.id=events.repo_id where events.actor_id = ? Order By events.id ASC";
-    await db.query(query, [actorId], function(err, rows) {
-      // res.status(200).render("events", { title: "All Events", events: rows });
-      const events = eventTransformers(rows);
-
-      res.send(events);
-
-      if (!err) console.log("The solution is: ");
-      else console.log("Error while performing Query.");
-    });
-  } catch (ex) {
-    return ex;
-  }
-};
-
-const deleteEvents = async function deleteEvents(req, res) {
-  try {
-    let query = " TRUNCATE TABLE events";
-    let actQuery = " TRUNCATE TABLE actors";
-    let repoQuery = " TRUNCATE TABLE repository";
-    await db.execute(query, function(err, result) {
-      if (err) throw err;
-      console.log(" Events Records Deleted !");
-    });
-    await db.execute(actQuery, function(err, result) {
-      console.log(" Actors Records Deleted !");
-    });
-    await db.execute(repoQuery, function(err, result) {
-      console.log(" Repository Records Deleted !");
-    });
-    res.send(true);
-  } catch (ex) {
-    return ex;
-  }
-};
 exports.getAllEvents = events;
 exports.createEvent = createEvent;
 exports.getActorsEvent = getActorsEvent;
